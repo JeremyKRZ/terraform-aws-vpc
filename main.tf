56c00d4e9da434d569dd3fa15e8f3469e919c058
JeremyKRZ <jeremy.krzeczowski77@gmail.com>
2022-10-19 12:10:06 +0200

    Create vpc

--- main.tf
+++ main.tf
@@ -4,3 +4,13 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.vpc-name
+    Environment = var.env
+    Owner = var.owner
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -2,3 +2,32 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "cidr_block"{
+  type = string
+  description = "Le block CIDR du vpc. /16 est recommandé"
+}
+
+variable "tags" {
+  type = map(string)
+  description = "liste de tags pour les ressources"
+  default = {
+    Name = "iaas-vpc"
+    Owner = "JeremyKRZ"
+  }
+}
+
+variable "env"{
+    type = string
+    description = "env du vpc"
+}
+
+variable "vpc-name"{
+    type = string
+    description = "nom du vpc"
+}
+
+variable "owner"{
+    type = string
+    description = "propriétaire du vpc"
+}
